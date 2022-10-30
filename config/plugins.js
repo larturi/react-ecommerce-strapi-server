1471519cb182039ffaf5df5841af32682065e16a
Leandro Arturi <lea.arturi@gmail.com>
2022-10-30 13:29:47 -0300

    Cambio AWS S3 por Cloudinary - Custom Folder

--- config/plugins.js
+++ config/plugins.js
@@ -19,16 +19,31 @@ module.exports = ({ env }) => ({
   //   },
   // },
 
+  // upload: {
+  //   provider: 'cloudinary',
+  //   providerOptions: {
+  //       cloud_name: env('CLOUDINARY_NAME'),
+  //       api_key: env('CLOUDINARY_KEY'),
+  //       api_secret: env('CLOUDINARY_SECRET'),
+  //   },
+  //   upload_config: {
+  //     folder: env('CLOUDINARY_FOLDER'),
+  //   }
+  // }
+
   upload: {
     provider: 'cloudinary',
     providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+      cloud_name: env('CLOUDINARY_NAME'),
+      api_key: env('CLOUDINARY_KEY'),
+      api_secret: env('CLOUDINARY_SECRET'),
     },
-    upload_config: {
-      folder: env('CLOUDINARY_FOLDER'),
-    }
-  }
+    actionOptions: {
+      upload: {
+        folder: env('CLOUDINARY_FOLDER'),
+      },
+      delete: {},
+    },
+  },
   
 });
